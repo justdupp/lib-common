cd53f1d78e9a6c6498f77c913f53af6eaba4b891
justdupp <hqxhj1990@163.com>
2018-04-14 09:35:04 +0800

    更新api

--- src/main/java/com/hecc/lib/api/HeccRequest.java
+++ src/main/java/com/hecc/lib/api/HeccRequest.java
@@ -1,9 +1,55 @@
 package com.hecc.lib.api;
 
+import com.alibaba.fastjson.JSON;
+import com.taobao.api.internal.util.WebUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
 /**
  * @Auther xuhoujun
- * @Description:
+ * @Description: 接口请求封装
  * @Date: Created In 下午2:09 on 2018/3/31.
  */
-public class HeccRequest {
+public abstract class HeccRequest<T extends HeccResponse> {
+
+    // TODO: 2018/3/31 将URL信息放入配置文件当中 
+    private static final String HECC_URL = "";
+    private static final Logger logger = LoggerFactory.getLogger(HeccRequest.class);
+    protected T response;
+
+    protected abstract Class<T> getResponseClass();
+
+    protected abstract String getApiName();
+
+    protected abstract Map<String, String> getTextParams();
+
+    public T getResponse() {
+        String rsp = this.getResponseString();
+        Class<T> clazz = getResponseClass();
+
+        this.response = JSON.parseObject(rsp, clazz);
+
+        Map<String, String> params = getTextParams();
+        this.response.setParams(params);
+        this.response.setBody(rsp);
+        return this.response;
+    }
+
+    private String getResponseString() {
+        Map<String, String> params = getTextParams();
+        String apiName = getApiName();
+        try {
+            return WebUtils.doGet(HECC_URL + apiName, params);
+        //    return WebUtil.doPost(HECC_URL + apiName, params);
+        } catch (Exception ex) {
+            logger.error("hecc api invoke error:", ex);
+            return "{\n" +
+                    "  \"errCode\": 500,\n" +
+                    "  \"description\": \"hecc api invoke error\",\n" +
+                    "  \"description_cn\": \"请求失败,请稍等重试\"\n" +
+                    "}";
+        }
+    }
 }
--- src/main/java/com/hecc/lib/api/HeccResponse.java
+++ src/main/java/com/hecc/lib/api/HeccResponse.java
@@ -1,9 +1,60 @@
 package com.hecc.lib.api;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.io.Serializable;
+import java.util.Map;
+
 /**
  * @Auther xuhoujun
- * @Description:
+ * @Description: 接口响应封装
  * @Date: Created In 下午2:17 on 2018/3/31.
  */
-public class HeccResponse {
+public class HeccResponse implements Serializable {
+
+    @JSONField(name = "errCode")
+    private String errorCode;
+
+    @JSONField(name = "description")
+    private String description;
+
+    private String body;
+
+    private Map<String, String> params;
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
+
+    public boolean isSuccess() {
+        return "0".equals(this.errorCode);
+    }
 }
