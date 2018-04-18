4c52927e814e3e3f003c0135f9558539621cc198
justdupp <hqxhj1990@163.com>
2018-04-18 21:07:30 +0800

    update

--- src/main/java/com/hecc/lib/common/ObjectUtils.java
+++ src/main/java/com/hecc/lib/common/ObjectUtils.java
@@ -91,4 +91,28 @@ public class ObjectUtils {
         }
         return null;
     }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    /**
+     * 安全类型转换。
+     *
+     * @param source  源类型实例。
+     * @param target  目标类。
+     * @param <T>     目标类型。
+     * @return 如成功，则源类型转换为目标类型，否则返回“null”。
+     */
+    public static <T> T safeCast(Object source, Class<T> target) {
+        if (source == null) {
+            return null;
+        }
+        if (target.isInstance(source)){
+            return target.cast(source);
+        }
+        return null;
+    }
+
 }
