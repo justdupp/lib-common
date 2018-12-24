1fdd1d6a03bc128c0ec887a3dc1478b5e3bca07b
xuhoujun <xuhoujun@111.com.cn>
2018-12-24 17:00:30 +0800

    test

--- src/main/java/com/hecc/lib/util/CoinChange.java
+++ src/main/java/com/hecc/lib/util/CoinChange.java
@@ -28,15 +28,6 @@ public class CoinChange {
         return result;
     }
 
-    /*public static String toChineseDigits(Integer money){
-        if(null == money || 0 == money){
-            return "";
-        }
-        String[] chineseDigits = new String[]{"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
-        String[] chineseUnits = new String[]{"圆", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿", "拾", "佰", "仟", "万", "拾", "佰", "仟"};
-        int[] inters = new int[money.toString().length()];
-    }
-*/
     /**
      * 返回最小值
      * @param i
